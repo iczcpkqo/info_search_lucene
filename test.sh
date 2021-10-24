d6b2fc72a7d79e953ce37e49726b475fb93f5285
Xiang_Mao <iczcpkqo@gmail.com>
2021-10-25 00:45:43 +0100

    add shell

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "--------------------------------------"
+echo "* Analyzers : STANDARD"
+echo "*   Scoring : BM25    ==============="
+echo "*                     || **BEST**  ||"
+echo "*   RESULTS :         ==============="
