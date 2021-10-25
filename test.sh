0deea32bb8de0ec28e8a94794aa565654bd06c53
Xiang_Mao <iczcpkqo@gmail.com>
2021-10-25 19:43:08 +0100

    update ignore

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "--------------------------------------"
-echo "* Analyzers : STANDARD"
-echo "*   Scoring : BM25    ==============="
-echo "*                     || **BEST**  ||"
-echo "*   RESULTS :         ==============="
